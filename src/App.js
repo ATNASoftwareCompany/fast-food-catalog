bf7864b27e12d71240245a2a35bf1c737ccc20a5
ATNA Software Group <taghizadeh0000@gmail.com>
2024-07-28 19:25:16 +0330

    first Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./Header/header";
+import CategoryList from "./CategoryList/categoryList";
+import { Fragment, useEffect, useState } from "react";
+import axios from "./axios";
+import Loading from "./Loading/loading";
+import FastFoodList from "./FastFoodList/fastFoodList";
+import SearchBar from "./SearchBar/searchBar";
+import notFound from "./assets/images/404.png";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [fastFoodItems, setFastFoods] = useState([]);
+
+  const fetchData = async (categoryId = null) => {
+    setLoading(true);
+    const response = await axios.get(
+      `/FastFood/List${categoryId ? "?categoryId=" + categoryId : ""}`
+    );
+    setLoading(false);
+    setFastFoods(response.data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const filterItems = (categoryId) => {
+    fetchData(categoryId);
+  };
+
+  const searchItems = async (term) => {
+    setLoading(true);
+    const response = await axios.get(
+      `/FastFood/search/${term ? "?term=" + term : ""}`
+    );
+    setLoading(false);
+    setFastFoods(response.data);
+  };
+  const renderContent = () => {
+    if (loading) {
+      return <Loading theme="dark" />;
+    }
+
+    if (fastFoodItems.length === 0) {
+      return (
+        <>
+          <div className="alert alert-warning text-center">
+            برای کلید واژه فوق هیچ مقداری یافت نشد
+          </div>
+          <img className="mx-auto mt-5 d-block fade-in-horiz" src={notFound} />
+        </>
+      );
+    }
+
+    return <FastFoodList fastFoodItems={fastFoodItems} />;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper bg-faded-dark">
+      <Header></Header>
+      <CategoryList filterItems={filterItems}>
+        <SearchBar searchItems={searchItems} />
+      </CategoryList>
+      <div className="container mt-4">{renderContent()}</div>
     </div>
   );
 }
---
+++ src/Header/header.jsx
@@ -0,0 +1,14 @@
+import './header.css';
+const Header = () => {
+  return (
+    <header className="bg-size-cover bg-position-center">
+      <div className="container">
+        <h1 className="text-start text-light text-uppercase py-5">
+          fast food catalog
+        </h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
